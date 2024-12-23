ebe088296b87d5d919d53c99eb806deb9c3f58a4
itututec <103866847+itututec@users.noreply.github.com>
2024-12-23 16:33:43 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://wcny68.itututec.eu.org' },
-    { title: '访问文档中心获取更多帮助', url: 'https://wcny68.itututec.eu.org' }
+    { title: '访问文档中心获取更多帮助', url: 'https://blog.tangly1024.com/about' }
   ],
 
   // 英雄区左右侧组件颠倒位置
